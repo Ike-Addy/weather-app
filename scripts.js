d6080d34c210597d9f7a3c8e506161e6841c3a5b
Ike Addy <ikaddy05@gmail.com>
2024-02-15 15:16:42 -0500

    Empty commit

--- scripts.js
+++ scripts.js
@@ -93,3 +93,8 @@ function displayHourlyForecast(hourlyData) {
         hourlyForecastDiv.innerHTML += hourlyItemHtml;
     });
 }
+
+function showImage() {
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.style.display = 'block';
+}
